be46182a44e1e3dd7b2800dc7a63f82603ac9546
Andrei Borodin <andrei.borodin@microsoft.com>
2019-08-28 15:23:57 -0700

    First commit of the setup

---
+++ Tests/ProjectGeneration/Consumer.2019.Unity/Assets/TestRunner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TestRunner : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshPro platformText;
+
+    [SerializeField]
+    private TextMeshPro resultsText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Tests/ProjectGeneration/Producer.2019.Unity/Assets/PlatformTest.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace Microsoft.Build.Unity.ProjectGeneration.Test
+{
+    public enum TestResult
+    {
+        Success,
+        Failure,
+        PlatformNotTested
+    }
+
+    public class PlatformTest
+    {
+        public string Platform => $"{Player}.{Configuration}";
+
+        private string Configuration =>
+#if UNITY_EDITOR
+            "InEditor";
+#else
+            "Player";
+#endif
+
+        private string Player =>
+#if UNITY_ANDROID
+            "Android";
+#elif UNITY_IPHONE
+            "iOS";
+#elif UNITY_WSA
+            "WSA";
+#elif UNITY_STANDALONE
+            "Standalone";
+#endif
+
+        public TestResult RunTest()
+        {
+#if UNITY_ANDROID
+
+#elif UNITY_IPHONE
+
+#elif UNITY_WSA
+
+#elif UNITY_STANDALONE
+
+#endif
+
+            return TestResult.PlatformNotTested;
+        }
+    }
+}
