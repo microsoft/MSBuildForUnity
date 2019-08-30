0193ac1146cce87a9d43c4d98560f55603173f6f
Andrei Borodin <andrei.borodin@microsoft.com>
2019-08-30 12:47:11 -0700

    Added more tests, and tested WSA player

--- Tests/ProjectGeneration/Producer.2019.Unity/Assets/PlatformTest.cs
+++ Tests/ProjectGeneration/Producer.2019.Unity/Assets/PlatformTest.cs
@@ -1,9 +1,12 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using System;
 using System.Linq;
 using UnityEngine;
+
+#if UNITY_WSA
+using System;
+#endif
 
 namespace Microsoft.Build.Unity.ProjectGeneration.Test
 {
@@ -46,15 +49,76 @@ namespace Microsoft.Build.Unity.ProjectGeneration.Test
             bool testedEditor = false;
             bool testedPlatform = false;
 
+            bool isEditor = false;
+#if UNITY_EDITOR
+            isEditor = true;
+            Debug.Log($"Found '{UnityEditor.AssetDatabase.GetAllAssetPaths().Length}' assets in the consuming project");
+            if (!UnityEditor.AssetDatabase.GetAllAssetPaths().Any(t => t.Contains("Microsoft.Build.Unity.ProjectGeneration.TestAssembly")))
+            {
+                Debug.LogError("Couldn't find this assembly (Microsoft.Build.Unity.ProjectGeneration.TestAssembly) as an asset of the consuming project.");
+                return TestResult.Failure;
+            }
+            testedEditor = true;
+#endif
+
+            if (!isEditor)
+            {
+                testedEditor = true;
+            }
+
 #if UNITY_ANDROID
+            AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            AndroidJavaObject unityActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
 
+            if (unityPlayer == null)
+            {
+                Debug.LogError("Failed to get the unity player class.");
+            }
+
+            if (isEditor)
+            {
+                Debug.Log($"We are in editor, compling android, but won't have activity.");
+            }
+            else if (unityActivity == null)
+            {
+                Debug.LogError($"Failed to get Unity Activity.");
+                return TestResult.Failure;
+            }
+
+            testedPlatform = true;
 #elif UNITY_IPHONE
+            string id = UnityEngine.iOS.Device.advertisingIdentifier;
+            Debug.Log($"Our iOS Advertising Id is '{id}'");
 
+            testedPlatform = true;
 #elif UNITY_WSA
+            using (UnityEngine.XR.WSA.Input.GestureRecognizer gestureRecognizer = new UnityEngine.XR.WSA.Input.GestureRecognizer())
+            {
+                Debug.Log($"We can recognize the following gestures '{gestureRecognizer.GetRecognizableGestures()}'");
+            }
+            System.Threading.Tasks.Task.Run(async () =>
+            {
+                try
+                {
+#if !UNITY_EDITOR
+                    Debug.Log("About to try creating a file");
+                    Windows.Storage.StorageFolder storageFolder = Windows.Storage.KnownFolders.PicturesLibrary;
+                    Windows.Storage.StorageFile file = await storageFolder.CreateFileAsync("sample.png", Windows.Storage.CreationCollisionOption.ReplaceExisting);
+                    Debug.Log($"Created sample.png, now will delete");
+                    await file.DeleteAsync();
+#endif
+                    await System.Threading.Tasks.Task.CompletedTask;
+                }
+                catch (Exception ex)
+                {
+                    Debug.LogError($"Encountered an error when trying to create/delete file: {ex.ToString()}");
+                }
+            }).Wait();
 
+            testedPlatform = true;
 #elif UNITY_STANDALONE
-            IntPtr handle = GetActiveWindow();
-            if (handle == IntPtr.Zero)
+            System.IntPtr handle = GetActiveWindow();
+            if (handle == System.IntPtr.Zero)
             {
                 Debug.LogError("Failed to get active window handle.");
                 return TestResult.Failure;
@@ -64,18 +128,6 @@ namespace Microsoft.Build.Unity.ProjectGeneration.Test
                 Debug.Log($"Handle of current active window is: {handle.ToInt64()}.");
             }
             testedPlatform = true;
-#endif
-
-#if UNITY_EDITOR
-            Debug.Log($"Found '{UnityEditor.AssetDatabase.GetAllAssetPaths().Length}' assets in the consuming project");
-            if (!UnityEditor.AssetDatabase.GetAllAssetPaths().Any(t => t.Contains("Microsoft.Build.Unity.ProjectGeneration.TestAssembly")))
-            {
-                Debug.LogError("Couldn't find this assembly (Microsoft.Build.Unity.ProjectGeneration.TestAssembly) as an asset of the consuming project.");
-                return TestResult.Failure;
-            }
-            testedEditor = true;
-#else
-            testedEditor = true;
 #endif
 
             return (testedEditor && testedPlatform) ? TestResult.Success : TestResult.PlatformNotTested;
