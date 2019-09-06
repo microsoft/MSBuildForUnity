794a787f8c88c32a274c157659ce0a1bddb71621
Andrei Borodin <andrei.borodin@microsoft.com>
2019-09-06 11:29:58 -0700

    Testing Projects for ProjectGenerator (#23)
    * First commit of the setup
    
    * Adding 2018 testing
    
    * Setting up the publsih directories to not be ignored
    
    * Removed unnecessary packages
    
    * Removed dlls from building
    
    * Fixed up .metas
    
    * Rearrange the assets a bit
    
    * Referenced the assets in 2018
    
    * added asmdef for testrunner
    
    * Additional updates
    
    * Added more tests, and tested WSA player
    
    * Setup the 2019 test project
    
    * Updated somethings with consumer
    
    * Additional changes

---
+++ Tests/ProjectGeneration/Consumer.2019.Unity/Assets/TestRunner/TestRunner.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Build.Unity.ProjectGeneration.Test;
+using System;
+using TMPro;
+using UnityEngine;
+
+public class TestRunner : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI platformText = null;
+
+    [SerializeField]
+    private TextMeshProUGUI resultsText = null;
+
+    private void Awake()
+    {
+        try
+        {
+            PlatformTest testClass = new PlatformTest();
+            platformText.text = testClass.Platform;
+
+            TestResult results = testClass.RunTest();
+            if (results == TestResult.Failure)
+            {
+                resultsText.color = Color.red;
+            }
+            else if (results == TestResult.PlatformNotTested)
+            {
+                resultsText.color = Color.yellow;
+            }
+            resultsText.text = results.ToString();
+        }
+        catch (Exception ex)
+        {
+            resultsText.text = $"Test Failed; exception:\r\n {ex.ToString()}";
+            resultsText.color = Color.red;
+        }
+    }
+}
---
+++ Tests/ProjectGeneration/Producer.2019.Unity/Assets/PlatformTest.cs
@@ -0,0 +1,136 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Linq;
+using UnityEngine;
+
+#if UNITY_WSA
+using System;
+#endif
+
+namespace Microsoft.Build.Unity.ProjectGeneration.Test
+{
+    public enum TestResult
+    {
+        Success,
+        Failure,
+        PlatformNotTested
+    }
+
+    public class PlatformTest
+    {
+        public string Platform => $"{Player}.{Configuration}";
+
+        private string Configuration =>
+#if UNITY_EDITOR
+            "InEditor";
+#else
+            "Player";
+#endif
+
+        private string Player =>
+#if UNITY_ANDROID
+            "Android";
+#elif UNITY_IPHONE
+            "iOS";
+#elif UNITY_WSA
+            "WSA";
+#elif UNITY_STANDALONE
+            "Standalone";
+#endif
+
+#if UNITY_STANDALONE
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern System.IntPtr GetActiveWindow();
+#endif
+
+        public TestResult RunTest()
+        {
+            bool testedEditor = false;
+            bool testedPlatform = false;
+
+            bool isEditor = false;
+#if UNITY_EDITOR
+            isEditor = true;
+            Debug.Log($"Found '{UnityEditor.AssetDatabase.GetAllAssetPaths().Length}' assets in the consuming project");
+            if (!UnityEditor.AssetDatabase.GetAllAssetPaths().Any(t => t.Contains("Microsoft.Build.Unity.ProjectGeneration.TestAssembly")))
+            {
+                Debug.LogError("Couldn't find this assembly (Microsoft.Build.Unity.ProjectGeneration.TestAssembly) as an asset of the consuming project.");
+                return TestResult.Failure;
+            }
+            testedEditor = true;
+#endif
+
+            if (!isEditor)
+            {
+                testedEditor = true;
+            }
+
+#if UNITY_ANDROID
+            AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            AndroidJavaObject unityActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+            if (unityPlayer == null)
+            {
+                Debug.LogError("Failed to get the unity player class.");
+            }
+
+            if (isEditor)
+            {
+                Debug.Log($"We are in editor, compling android, but won't have activity.");
+            }
+            else if (unityActivity == null)
+            {
+                Debug.LogError($"Failed to get Unity Activity.");
+                return TestResult.Failure;
+            }
+
+            testedPlatform = true;
+#elif UNITY_IPHONE
+            string id = UnityEngine.iOS.Device.advertisingIdentifier;
+            Debug.Log($"Our iOS Advertising Id is '{id}'");
+
+            testedPlatform = true;
+#elif UNITY_WSA
+            using (UnityEngine.XR.WSA.Input.GestureRecognizer gestureRecognizer = new UnityEngine.XR.WSA.Input.GestureRecognizer())
+            {
+                Debug.Log($"We can recognize the following gestures '{gestureRecognizer.GetRecognizableGestures()}'");
+            }
+            System.Threading.Tasks.Task.Run(async () =>
+            {
+                try
+                {
+#if !UNITY_EDITOR
+                    Debug.Log("About to try creating a file");
+                    Windows.Storage.StorageFolder storageFolder = Windows.Storage.KnownFolders.PicturesLibrary;
+                    Windows.Storage.StorageFile file = await storageFolder.CreateFileAsync("sample.png", Windows.Storage.CreationCollisionOption.ReplaceExisting);
+                    Debug.Log($"Created sample.png, now will delete");
+                    await file.DeleteAsync();
+#endif
+                    await System.Threading.Tasks.Task.CompletedTask;
+                }
+                catch (Exception ex)
+                {
+                    Debug.LogError($"Encountered an error when trying to create/delete file: {ex.ToString()}");
+                }
+            }).Wait();
+
+            testedPlatform = true;
+#elif UNITY_STANDALONE
+            System.IntPtr handle = GetActiveWindow();
+            if (handle == System.IntPtr.Zero)
+            {
+                Debug.LogError("Failed to get active window handle.");
+                return TestResult.Failure;
+            }
+            else
+            {
+                Debug.Log($"Handle of current active window is: {handle.ToInt64()}.");
+            }
+            testedPlatform = true;
+#endif
+
+            return (testedEditor && testedPlatform) ? TestResult.Success : TestResult.PlatformNotTested;
+        }
+    }
+}
