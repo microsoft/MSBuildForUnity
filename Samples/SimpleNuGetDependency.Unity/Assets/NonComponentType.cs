9c6b379f9e556073d086d31ebf308cf04c9c79ad
Andrei Borodin <andrei.borodin@microsoft.com>
2019-11-06 12:50:36 -0800

    Made use of JSOn

--- Samples/SimpleNuGetDependency.Unity/Assets/NonComponentType.cs
+++ Samples/SimpleNuGetDependency.Unity/Assets/NonComponentType.cs
@@ -7,12 +7,6 @@ public class NonComponentType : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log(Newtonsoft.Json.ConstructorHandling.AllowNonPublicDefaultConstructor);
     }
 }
