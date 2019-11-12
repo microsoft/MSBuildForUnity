0b41d2c9f865cd38e7b8bc3f4ad39986846d3a37
Andrei Borodin <andrei.borodin@microsoft.com>
2019-11-12 15:02:32 -0800

    Fixed up most of the things needed to make supported integrated dependencies

--- Samples/IntegratedDependencies.Unity/Assets/NonComponentType.cs
+++ Samples/IntegratedDependencies.Unity/Assets/NonComponentType.cs
@@ -1,17 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
 using UnityEngine;
 
 public class NonComponentType : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
     {
         
     }
