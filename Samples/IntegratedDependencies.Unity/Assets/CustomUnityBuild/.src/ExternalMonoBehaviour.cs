4e7b3de70a7b11223e99af47a049d9d2b961347b
Andrei Borodin <anborod@microsoft.com>
2019-11-19 20:51:10 -0800

    Checking in a C# project file that as part of Integrated dependencies will build it's source externally.

---
+++ Samples/IntegratedDependencies.Unity/Assets/CustomUnityBuild/.src/ExternalMonoBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExternalMonoBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(typeof(ExternalMonoBehaviour).BaseType.FullName);
+    }
+}
