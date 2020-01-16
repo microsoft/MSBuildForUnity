852062243f8eaef8f736e0482e0d7dd0644cc8d4
Andrei Borodin <andrei.borodin@microsoft.com>
2020-01-15 17:36:09 -0800

    Seems to work.

---
+++ Samples/CrossUnityDependencies.Unity/Assets/CrossUnityTestScript.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine;
+
+public class CrossUnityTestScript : MonoBehaviour
+{
+    private GeneralComponentType generalComponentType = new GeneralComponentType();
+
+    private void Start()
+    {
+        Debug.Log(generalComponentType.GetThisDataToo());
+    }
+}
---
+++ Samples/IntegratedDependencies.Unity/Assets/GeneralComponent/GeneralComponentType.cs
@@ -0,0 +1,16 @@
+﻿using CommonLibrary;
+using UnityEngine;
+
+public class GeneralComponentType : MonoBehaviour
+{
+    private CommonComponent commonComponent = new CommonComponent();
+    private void Start()
+    {
+        Debug.Log(commonComponent.GetData());
+    }
+
+    public string GetThisDataToo()
+    {
+        return "IntegartedDependencies Project + {commonComponent.GetData()}";
+    }
+}
